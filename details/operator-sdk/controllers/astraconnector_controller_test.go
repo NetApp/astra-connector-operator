42a3a6941265331ef158b43da71774da52246bc6
Cole Palm <cole.palm@netapp.com>
2023-07-28 14:59:20 -0600

    validating all resources

--- details/operator-sdk/controllers/astraconnector_controller_test.go
+++ details/operator-sdk/controllers/astraconnector_controller_test.go
@@ -6,16 +6,19 @@ package controllers
 
 import (
 	"context"
-	v1 "github.com/NetApp-Polaris/astra-connector-operator/details/operator-sdk/api/v1"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"time"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	v1 "github.com/NetApp-Polaris/astra-connector-operator/details/operator-sdk/api/v1"
 )
 
 var _ = Describe("Astraconnector controller", func() {
@@ -46,8 +49,8 @@ var _ = Describe("Astraconnector controller", func() {
 
 		It("should successfully reconcile a custom resource for astraconnector", func() {
 			By("creating the custom resource for the astraconnector")
-			astraconnector := &v1.AstraConnector{}
-			err := k8sClient.Get(ctx, typeNamespaceName, astraconnector)
+			astraConnector := &v1.AstraConnector{}
+			err := k8sClient.Get(ctx, typeNamespaceName, astraConnector)
 
 			if err != nil && errors.IsNotFound(err) {
 				// Mocking CR
@@ -61,7 +64,6 @@ var _ = Describe("Astraconnector controller", func() {
 							Name:   "test-registry",
 							Secret: "test-secret",
 						},
-
 						AstraConnect: v1.AstraConnect{
 							Image:    "test-image",
 							Replicas: 3,
@@ -90,16 +92,18 @@ var _ = Describe("Astraconnector controller", func() {
 			})
 			Expect(err).To(Not(HaveOccurred()))
 
-			By("Checking if Deployment was successfully created in the reconciliation")
+			By("Checking if natssync-client deployment was successfully created in the reconciliation")
 			Eventually(func() error {
 				found := &appsv1.Deployment{}
-				return k8sClient.Get(ctx, typeNamespaceName, found)
+				natsSyncName := types.NamespacedName{Name: "natssync-client", Namespace: connectorName}
+				return k8sClient.Get(ctx, natsSyncName, found)
 			}, time.Minute, time.Second).Should(Succeed())
 
-			By("Checking the latest Status Condition added to the AstraConnector instance")
+			By("Checking if nats statefulset was successfully created in the reconciliation")
 			Eventually(func() error {
-				// TODO
-				return nil
+				found := &appsv1.StatefulSet{}
+				natsName := types.NamespacedName{Name: "nats", Namespace: connectorName}
+				return k8sClient.Get(ctx, natsName, found)
 			}, time.Minute, time.Second).Should(Succeed())
 		})
 	})
