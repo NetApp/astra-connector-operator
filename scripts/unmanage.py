1ceb40e254dd9c3fe91ff179670f764f10551f22
Cole Palm <cole.palm@netapp.com>
2023-08-11 15:19:54 -0600

    fetch cloud id

--- scripts/unmanage.py
+++ scripts/unmanage.py
@@ -1,39 +1,39 @@
 import argparse
-from typing import Tuple
+import json
 import requests
-from requests.models import Response
 
 
-def main(cloud_bridge_host, cloud_id, cluster_id):
-    print(f"Using Cloud Bridge Host: {cloud_bridge_host}")
-    print(f"Using Cloud ID: {cloud_id}")
-    print(f"Using Cluster ID: {cluster_id}")
+def main(astra_host, account_id, auth_token):
+    print(f"Using Cloud Bridge Host: {astra_host}")
+    print(f"Using Account ID: {account_id}")
 
-    remove_cluster(cloud_bridge_host, cloud_id, cluster_id)
+    remove_cluster(astra_host, account_id, auth_token)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Unmanage Astra Conncetor")
     parser.add_argument(
-        "--cloudBridgeHost",
+        "--astraHost",
         default="astra.netapp.io",
         help="Host for the Cloud Bridge (default: astra.netapp.io)",
     )
     parser.add_argument(
-        "--cloudId",
-        help="ID of Cloud that is being used for unregistration",
+        "--accountId",
+        help="ID of account that is being used for unregistration",
     )
     parser.add_argument(
-        "--clusterId",
-        help="ID of north side cluster that is being used for unregistered from",
+        "--authToken",
+        help="Token being used for authentication",
     )
 
     args = parser.parse_args()
-    main(args.cloudBridgeUrl, args.cloudId, args.clusterId)
+    main(args.astraHost, args.accountId, args.authToken)
 
 
-def remove_cluster(self, astra_host: str, cloud_id: str, cluster_id: str) -> Tuple[Response, Exception]:
-    url = f"{astra_host}/accounts/{self.astra_installer['spec']['connectorSpec']['astra']['accountID']}/topology/v1" \
+def remove_cluster(self, astra_host: str, account_id: str, token: str) -> None:
+    cloud_id = get_cloud_id(astra_host, token)
+
+    url = f"{astra_host}/accounts/{account_id}/topology/v1" \
           f"/clouds/{cloud_id}/clusters/{cluster_id}"
     self.log.info("Removing Cluster", extra={"clusterID": cluster_id})
     headers = {"Authorization": f"Bearer {self.astra_installer['spec']['connectorSpec']['astra']['token']}"}
@@ -48,3 +48,27 @@ def remove_cluster(self, astra_host: str, cloud_id: str, cluster_id: str) -> Tup
         return None, Exception(f"Remove cluster failed with statusCode: {response.status_code}")
 
     return response, None
+
+
+def get_cloud_id(self, astra_host: str, account_id: str, token: str) -> str:
+    try:
+        response = self.list_clouds(astra_host, account_id, token)
+        resp = response.json()
+        cloud_id = next((item["id"] for item in resp["items"] if item["cloudType"] == "private"), "")
+        return cloud_id
+    except (requests.exceptions.RequestException, json.JSONDecodeError, KeyError) as err:
+        self.log.error(f"Error listing clouds: {err}")
+        raise err
+
+
+def list_clouds(self, astra_host: str, account_id: str, token: str) -> requests.Response:
+    url = f"{astra_host}/accounts/{account_id}/topology/v1/clouds"
+
+    self.log.info("Getting clouds")
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response
