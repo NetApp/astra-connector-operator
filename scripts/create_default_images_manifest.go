0c2bb9fc8cf57c97755a64761adf2f174254b91f
jharrod <jharrod@users.noreply.github.com>
2023-08-11 14:08:02 -0600

    add manifest

---
+++ scripts/create_default_images_manifest.go
@@ -0,0 +1,50 @@
+/*
+Used in GitHub actions to generate a manifest of default images deployed by connector-operator
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/NetApp-Polaris/astra-connector-operator/common"
+)
+
+func main() {
+	// Get cmd line args
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run create_default_images_manifest.go <output_file_path>")
+		os.Exit(1)
+	}
+	destinationFilePath := os.Args[1]
+
+	fmt.Printf("Creating default-image manifest file %s\n", destinationFilePath)
+
+	// Gather the full image name and versions
+	defaultImageRegistry := common.DefaultImageRegistry
+	images := []string{
+		fmt.Sprintf("%s/%s", defaultImageRegistry, common.AstraConnectDefaultImage),
+		fmt.Sprintf("%s/%s", defaultImageRegistry, common.NatsSyncClientDefaultImage),
+		fmt.Sprintf("%s/%s", defaultImageRegistry, common.NatsDefaultImage),
+		fmt.Sprintf("%s/%s", defaultImageRegistry, common.NeptuneDefaultImage),
+	}
+
+	// Open the manifest file
+	file, err := os.OpenFile(destinationFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Printf("error opening output file for writing: %s\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Write the file
+	for _, image := range images {
+		// Write the string content to the file
+		_, err = file.WriteString(fmt.Sprintf("%s\n", image))
+		if err != nil {
+			fmt.Printf("error writing to file: %s\n", err)
+			os.Exit(1)
+		}
+	}
+
+}
