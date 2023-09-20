e9e4c562a0468b327885d9a2752f6744d3ce211a
GitHub Action <action@github.com>
2023-09-20 20:49:54 +0000

    Add generated files

--- common/constants.go
+++ common/constants.go
@@ -9,9 +9,10 @@ import "github.com/NetApp-Polaris/astra-connector-operator/app/conf"
 const (
 	DefaultImageRegistry = "netappdownloads.jfrog.io/docker-astra-control-staging/arch30/neptune"
 
-	AstraConnectName            = "astraconnect"
-	AstraConnectDefaultReplicas = 1
-	AstraConnectDefaultImage    = "astra-connector:1.0.202309201645"
+	AstraConnectName                 = "astraconnect"
+	AstraConnectDefaultReplicas      = 1
+	AstraConnectDefaultImage         = "astra-connector:1.0.202309201645"
+	AstraConnectorOperatorRepository = "netapp/astra-connector-operator"
 
 	NatsSyncClientName                  = "natssync-client"
 	NatsSyncClientDefaultReplicas       = 1
