0cdde2ba3bd8fe46f3cb892ebcb3f8010d866b20
Alice Paquette <palice@netapp.com>
2023-12-12 12:22:46 -0500

    ASTRACTL-30644: make change backwards compatible just so polaris PR can merge without breaking things

--- common/constants.go
+++ common/constants.go
@@ -56,8 +56,9 @@ const (
 	AstraPrivateCloudType = "private"
 	AstraPrivateCloudName = "private"
 
-	ConnectorNeptuneCapability = "neptuneV1"
 	ConnectorV2Capability      = "connectorV2" // V2 refers specifically to Arch 3.0 connector and beyond
+	ConnectorNeptuneCapability = "neptuneV1"
+	ConnectorRelayCapability   = "relayV1" // TODO ASTRACTL-30644: remove
 	ConnectorWatcherCapability = "watcherV1"
 
 	AstraClustersAPIVersion        = "1.4"
@@ -71,6 +72,7 @@ func GetNeptuneRepositories() []string {
 func GetConnectorCapabilities() []string {
 	capabilities := []string{
 		ConnectorV2Capability,
+		ConnectorRelayCapability, // TODO ASTRACTL-30644: remove
 		ConnectorWatcherCapability,
 	}
 
