0ec74d73993c0d5702d5b5fae26c04d49957c525
Joe Webster <31218426+jwebster7@users.noreply.github.com>
2024-01-26 15:29:04 -0600

    Add DNS 1123 validation for connector host cluster names (#223)

--- details/operator-sdk/api/v1/astraconnector_validator_test.go
+++ details/operator-sdk/api/v1/astraconnector_validator_test.go
@@ -8,22 +8,42 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "github.com/NetApp-Polaris/astra-connector-operator/details/operator-sdk/api/v1"
 )
 
-func TestAstraConnector_ValidateCreateAstraConnector(t *testing.T) {
-	ai := &v1.AstraConnector{}
-	err := ai.ValidateCreateAstraConnector()
-
-	// Validate that no error occurred
-	if len(err) != 0 {
-		t.Errorf("Expected no errors, but got %d", len(err))
+// makeAstraConnector is a test helper that creates an Astra Connector with default values.
+func makeAstraConnector(t *testing.T) *v1.AstraConnector {
+	t.Helper()
+	return &v1.AstraConnector{
+		ObjectMeta: v12.ObjectMeta{
+			Name:            "astra-connector",
+			Namespace:       "astra-connector",
+			ResourceVersion: "v1",
+		},
+		Spec: v1.AstraConnectorSpec{
+			Astra: v1.Astra{
+				ClusterName: "managed-cluster",
+			},
+		},
 	}
 }
 
+func TestAstraConnector_ValidateCreateAstraConnector(t *testing.T) {
+	// Validate that no error occurred.
+	ai := makeAstraConnector(t)
+	err := ai.ValidateCreateAstraConnector()
+	assert.Emptyf(t, err, "expected empty error list, but got %v", err)
+
+	// Test with an invalid cluster name and ensure an error occurred.
+	ai.Spec.Astra.ClusterName = "INVALID-CLUSTER-NAME"
+	err = ai.ValidateCreateAstraConnector()
+	assert.NotEmptyf(t, err, "expected non-empty error list, but got %v", err)
+}
+
 func TestAstraConnector_ValidateUpdateAstraConnector(t *testing.T) {
-	ai := &v1.AstraConnector{}
+	ai := makeAstraConnector(t)
 	err := ai.ValidateUpdateAstraConnector()
 
 	// Validate that no error occurred
@@ -33,7 +53,7 @@ func TestAstraConnector_ValidateUpdateAstraConnector(t *testing.T) {
 }
 
 func TestAstraConnector_ValidateNamespace(t *testing.T) {
-	ai := &v1.AstraConnector{}
+	ai := makeAstraConnector(t)
 	ai.ObjectMeta.Namespace = "default"
 
 	errors := ai.ValidateCreateAstraConnector()
--- util/go_util_test.go
+++ util/go_util_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
 
 	v1 "github.com/NetApp-Polaris/astra-connector-operator/details/operator-sdk/api/v1"
 	"github.com/NetApp-Polaris/astra-connector-operator/util"
@@ -82,4 +83,71 @@ func TestGetJSONFieldName(t *testing.T) {
 		jsonTag = util.GetJSONFieldName(&ac.Status, &a.field)
 		assert.Equal(t, "", jsonTag)
 	})
+
+	t.Run("TestGetJSONFieldName__WhenValidStructFieldIsNested", func(t *testing.T) {
+		ac := createAstraConnector()
+
+		jsonTag := util.GetJSONFieldName(&ac.Spec.Astra, &ac.Spec.Astra.ClusterName)
+		assert.Equal(t, "clusterName", jsonTag)
+	})
+}
+
+func TestIsValidDNS1123Label(t *testing.T) {
+	tests := map[string]struct {
+		input    string
+		expected bool
+	}{
+		"input contains a number at end": {
+			"valid-astra-cluster-input-0",
+			true,
+		},
+		"input contains a number at beginning": {
+			// "-" is not valid for end of Kubernetes names.
+			"0-valid-astra-cluster-input",
+			true,
+		},
+		"input is within the max character count": {
+			rand.String(63),
+			true,
+		},
+		"input is greater than the max character count": {
+			rand.String(64),
+			false,
+		},
+		"input is empty": {
+			// "" is not valid for Kubernetes names.
+			"",
+			false,
+		},
+		"input contains illegal character at beginning": {
+			// "-" is not valid for end of Kubernetes names.
+			"-invalid-astra-cluster-input",
+			false,
+		},
+
+		"input contains illegal character within": {
+			// "_" is not valid for Kubernetes names.
+			"invalid-astra-cluster_name",
+			false,
+		},
+		"input contains illegal character at end": {
+			// "-" is not valid for end of Kubernetes names.
+			"invalid-astra-cluster-input-",
+			false,
+		},
+		"input contains illegal uppercase character at beginning": {
+			"Invalid-astra-cluster-input",
+			false,
+		},
+		"input is a dns subdomain and not a input": {
+			"example.com",
+			false,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.expected, util.IsValidDNS1123Label(test.input))
+		})
+	}
 }
