b3aaa1459a4a1a47ebf7e40dfe2d976f996969b3
jharrod <jharrod@users.noreply.github.com>
2024-02-08 18:08:08 -0700

    save

--- python_tests/management_tests/test_neptune.py
+++ python_tests/management_tests/test_neptune.py
@@ -2,21 +2,21 @@ import uuid
 from python_tests import defaults
 
 
-def test_create_app_vault(app_clusters, shared_bucket):
+def test_create_app_vault(app_clusters):
     secret_name = f"app-vault-secret-{str(uuid.uuid4())[:8]}"
     app_clusters[0].cr_helper.app_vault.create_app_vault_secret(
         namespace=defaults.DEFAULT_CONNECTOR_NAMESPACE,
         secret_name=secret_name,
-        access_key=shared_bucket.access_key,
-        secret_key=shared_bucket.secret_key
+        access_key=app_clusters[0].default_test_bucket.access_key,
+        secret_key=app_clusters[0].default_test_bucket.secret_key
     )
 
     app_vault_name = f"test-app-vault-{str(uuid.uuid4())[:8]}"
     app_clusters[0].cr_helper.app_vault.create_app_vault(
         name=app_vault_name,
         namespace=defaults.DEFAULT_CONNECTOR_NAMESPACE,
-        bucket_name=shared_bucket.bucket_name,
-        bucket_host=shared_bucket.host,
+        bucket_name=app_clusters[0].default_test_bucket.bucket_name,
+        bucket_host=app_clusters[0].default_test_bucket.host,
         secret_name=secret_name,
         provider_type="generic-s3"
     )
