cd51a5e7bc1a85cde6d766888e6b4375eb2c60b9
Oscar Rodriguez <oscarr@netapp.com>
2024-05-13 17:01:00 -0600

    adding unit test

--- app/conf/config_test.go
+++ app/conf/config_test.go
@@ -1,10 +1,9 @@
 package conf_test
 
 import (
-	"testing"
-
 	"github.com/NetApp-Polaris/astra-connector-operator/app/conf"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestGetPodSecurityContext(t *testing.T) {
@@ -19,4 +18,35 @@ func TestGetSecurityContext(t *testing.T) {
 	assert.True(t, *sc.RunAsNonRoot)
 	assert.NotEqual(t, int64(0), *sc.RunAsUser)
 	assert.True(t, *sc.ReadOnlyRootFilesystem)
+}
+
+func TestImmutableConfiguration(t *testing.T) {
+	// Initialize a test configuration
+	config := conf.ImmutableConfiguration{}
+
+	// Test each method
+	if config.Host() != "" {
+		t.Errorf("Expected empty, got %s", config.Host())
+	}
+
+	if config.AppRoot() != "" {
+		t.Errorf("Expected empty, got %s", config.AppRoot())
+	}
+
+	// TODO ADD test
+}
+
+func TestImmutableFeatureFlags(t *testing.T) {
+	// Initialize a test feature flag configuration
+	flags := conf.ImmutableFeatureFlags{}
+
+	if flags.DeployNatsConnector() != false {
+		t.Errorf("Expected true, got %v", flags.DeployNatsConnector())
+	}
+
+	if flags.DeployNeptune() != false {
+		t.Errorf("Expected false, got %v", flags.DeployNeptune())
+	}
+
+	// TODO add test
 }
