9e72dae66d98f5f783adea9373d21bcba3e83901
Oscar Rodriguez <oscarr@netapp.com>
2024-06-05 14:22:00 -0600

    removing extra files i accidently added

--- unified-installer/tmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-user="NetApp"
-repo="astra-connector-operator"
-
-latest_release=$(curl --silent "https://api.github.com/repos/$user/$repo/releases/latest" | jq -r .tag_name)
-
-echo "Latest release is: $latest_release"
